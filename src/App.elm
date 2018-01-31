5440ce8444a93d7fe4858855e88d505b5a38a9b0
Andreas Köberle <koeberle@gmail.com>
2018-01-31 12:45:14 +0100

    make it work

--- src/App.elm
+++ src/App.elm
@@ -5,10 +5,28 @@ import Html.Events exposing (onClick)
 import Random
 import OpenSolid.Point2d as Point2d
 import OpenSolid.Vector2d as Vector2d
-import OpenSolid.Svg exposing (point2d)
+import OpenSolid.Svg exposing (point2dWith, lineSegment2d)
 import Svg
 import Svg.Attributes as Attributes
 import Dict
+import OpenSolid.LineSegment2d as Line
+import List.Extra exposing (lift2)
+
+
+hormoneRange =
+    90
+
+
+branchLength =
+    5
+
+
+budRange =
+    2
+
+
+numberPoints =
+    2000
 
 
 randomPoint : Random.Generator ( Float, Float )
@@ -49,6 +67,8 @@ type alias Hormones =
 type alias Model =
     { hormons : Hormones
     , buds : Buds
+    , lines : List Line.LineSegment2d
+    , seed : Random.Seed
     }
 
 
@@ -56,8 +76,10 @@ init : ( Model, Cmd Msg )
 init =
     ( { hormons = []
       , buds =
-            [ Point2d.fromCoordinates ( 0, 0 )
+            [ Point2d.fromCoordinates ( 300, 300 )
             ]
+      , lines = []
+      , seed = Random.initialSeed 1
       }
     , Cmd.none
     )
@@ -66,19 +88,24 @@ init =
 view model =
     div []
         [ button [ onClick CreateHormons ] [ text "Generate" ]
+        , button [ onClick Grid ] [ text "Grid" ]
         , button [ onClick Step ] [ text "Step" ]
         , Svg.svg
             [ Attributes.width "600", Attributes.height "600", Attributes.viewBox "0 0 600 600" ]
             [ Svg.g
-                [ Attributes.stroke "black"
-                , Attributes.fill "white"
+                [ Attributes.stroke "none"
+                , Attributes.fill "HSLA(68, 63%, 37%, 1.00)"
                 ]
-                (List.map (point2d []) model.hormons)
+                (List.map (point2dWith { radius = 1 } []) model.hormons)
             , Svg.g
-                [ Attributes.stroke "red"
-                , Attributes.fill "white"
+                [ Attributes.stroke "#F7C92E"
+                , Attributes.fill "none"
                 ]
-                (List.map (point2d []) model.buds)
+                (List.map (point2dWith { radius = budRange } []) model.buds)
+            , Svg.g
+                [ Attributes.stroke "HSLA(312, 69%, 17%, 1.00)"
+                ]
+                (List.map (lineSegment2d []) model.lines)
             ]
         , div [] [ text (toString model) ]
         ]
@@ -88,15 +115,39 @@ type Msg
     = CreateHormons
     | NewHormons (List ( Float, Float ))
     | Step
+    | Grid
 
 
 update msg model =
     case msg of
         CreateHormons ->
-            ( model, (Random.generate NewHormons (randomPoints 1000)) )
+            ( model, (Random.generate NewHormons (randomPoints numberPoints)) )
 
         NewHormons hormons ->
-            ( { model | hormons = List.map Point2d.fromCoordinates hormons }, Cmd.none )
+            ( { model
+                | hormons = List.map Point2d.fromCoordinates hormons
+                , buds =
+                    [ Point2d.fromCoordinates ( 300.01, 300.04 )
+                    ]
+                , lines = []
+              }
+            , Cmd.none
+            )
+
+        Grid ->
+            let
+                range =
+                    List.range 0 50 |> List.map (toFloat >> ((*) 10))
+            in
+                ( { model
+                    | hormons = List.map Point2d.fromCoordinates (lift2 (,) range range)
+                    , buds =
+                        [ Point2d.fromCoordinates ( 300, 300 )
+                        ]
+                    , lines = []
+                  }
+                , Cmd.none
+                )
 
         Step ->
             ( step model, Cmd.none )
@@ -105,7 +156,7 @@ update msg model =
 findNearestBud : List Bud -> Hormone -> Maybe ( Hormone, Bud )
 findNearestBud buds h =
     (List.map (\b -> ( b, (Point2d.distanceFrom h b) )) buds)
-        |> (List.filter (\( _, d ) -> d < 40))
+        |> (List.filter (\( _, d ) -> d < hormoneRange))
         |> (List.sortBy Tuple.second)
         |> List.head
         |> Maybe.map (\b -> ( h, Tuple.first b ))
@@ -121,21 +172,48 @@ updateBud h b =
             Just { bud | vec = Vector2d.sum bud.vec (Vector2d.from bud.pos h) }
 
 
-calcNewPos ( _, { pos, vec } ) =
-    let
-        a =
-            Debug.log "" (Vector2d.normalize vec |> Vector2d.scaleBy 10)
-    in
-        Point2d.translateBy (Vector2d.normalize vec |> Vector2d.scaleBy 40) pos
+calcNewPos seed ( _, { pos, vec } ) =
+    ( pos
+    , Point2d.translateBy
+        (Vector2d.normalize vec
+            |> Vector2d.scaleBy
+                (Tuple.first (Random.step (Random.float 1 branchLength) seed))
+        )
+        pos
+    )
 
 
-isNotNearBud : Buds -> Hormone -> Bool
 isNotNearBud buds h =
-    (List.any (\b -> (Point2d.distanceFrom h b) > 20) buds)
+    (List.all (\( _, b ) -> (Point2d.distanceFrom h b) > budRange) buds)
+
+
+canMove ( _, { pos, vec } ) =
+    vec /= Vector2d.fromComponents ( 0, 0 )
+
+
+createLines b =
+    List.map (\l -> Line.fromEndpoints l) b
+
+
+branchBud : Bud -> ( Random.Seed, Buds ) -> ( Random.Seed, Buds )
+branchBud bud ( seed, buds ) =
+    let
+        ( v, newSeed ) =
+            Random.step (Random.int 0 10) seed
+    in
+        if (v > 7) then
+            ( newSeed, bud :: buds )
+        else
+            ( newSeed, buds )
+
+
+branchBuds : Buds -> Random.Seed -> ( Random.Seed, Buds )
+branchBuds buds seed =
+    (List.foldl branchBud ( seed, [] ) buds)
 
 
 step : Model -> Model
-step { hormons, buds } =
+step { hormons, buds, lines, seed } =
     let
         bs =
             List.map (\b -> ( (Point2d.coordinates b), { pos = b, vec = Vector2d.fromComponents ( 0, 0 ) } )) buds
@@ -146,6 +224,14 @@ step { hormons, buds } =
                 |> List.filterMap identity
                 |> List.foldl (\( h, b ) r -> Dict.update (Point2d.coordinates b) (updateBud h) r) bs
                 |> Dict.toList
-                |> List.map calcNewPos
+                |> List.filter canMove
+                |> List.map (calcNewPos seed)
+
+        ( newSeed, newBuds ) =
+            branchBuds buds seed
     in
-        { hormons = List.filter (isNotNearBud nearestBuds) hormons, buds = nearestBuds }
+        { hormons = List.filter (isNotNearBud nearestBuds) hormons
+        , buds = List.append newBuds (List.map Tuple.second nearestBuds)
+        , lines = List.append lines (createLines nearestBuds)
+        , seed = newSeed
+        }
